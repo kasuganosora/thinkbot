ab7cf2fea6b19efb86c3833ee50650b9c2a86dda
KasuganoSora <kennyjks@gmail.com>
2026-07-03 20:12:13 +0800

    chore: set vite build output directly to ../static

--- web/vite.config.js
+++ web/vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    outDir: '../static',
+    emptyOutDir: true
+  },
   server: {
     host: '0.0.0.0',
     port: 54727
